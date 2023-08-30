d11a81653676efe1c9e2b4501928e336a2f398a6
gejiang <632651021@qq.com>
2023-08-30 22:17:55 +0800

    补充代码

--- sky-server/src/main/java/com/sky/service/SetmealService.java
+++ sky-server/src/main/java/com/sky/service/SetmealService.java
@@ -45,6 +45,10 @@ public interface SetmealService {
      */
     void saveWithDish(SetmealDTO setmealDTO);
 
+    /**
+     * 批量删除套餐
+     * @param ids
+     */
     void deleteBatch(List<Long> ids);
 
     /**
